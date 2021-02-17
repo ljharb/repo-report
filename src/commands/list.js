403042aa888b29d368058dfae8962372e6e23e2b
Hani Mohammed <hani.m17@iiits.in>
2021-02-17 23:25:11 +0530

    Migrate to Yargs

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,65 +1,60 @@
-const { Command, flags } = require("@oclif/command");
-const { graphql } = require("@octokit/graphql");
-const logSymbols = require("log-symbols");
-require("dotenv").config();
+const { graphql } = require('@octokit/graphql');
+const logSymbols = require('log-symbols');
+const Table = require('cli-table');
 
-const Table = require("cli-table");
+const list = async (flags) => {
+	if (!process.env.GITHUB_PAT) {
+		console.log(`${logSymbols.error} env variable GITHUB_PAT not found`);
+	}
 
-class List extends Command {
-  async run() {
-    if (!process.env.GITHUB_PAT) {
-      this.log(`${logSymbols.error} env variable GITHUB_PAT not found`);
-    }
-    // Field names and their extraction method to be used on the query result
-    const fields = ["Repository", "Owner", "Access", "DefBranch", "isPublic"];
-    const mappedFields = [
-      (item) => item.name,
-      (item) => item.owner.login,
-      (item) => item.viewerPermission,
-      (item) => (item.defaultBranchRef ? item.defaultBranchRef.name : "---"),
-      (item) => (item.isPrivate ? logSymbols.error : logSymbols.success),
-    ];
+	// Field names and their extraction method to be used on the query result
+	const fields = ['Repository', 'Owner', 'Access', 'DefBranch', 'isPublic'];
+	const mappedFields = [
+		(item) => item.name,
+		(item) => item.owner.login,
+		(item) => item.viewerPermission,
+		(item) => (item.defaultBranchRef ? item.defaultBranchRef.name : '---'),
+		(item) => (item.isPrivate ? logSymbols.error : logSymbols.success),
+	];
 
-    const { flags } = this.parse(List);
+	// List available fields
+	if (flags.f) {
+		fields.map((item) => console.log(`- ${item}`));
+		return;
+	}
 
-    // List available fields
-    if (flags.fields) {
-      fields.map((item) => this.log(`- ${item}`));
-      return;
-    }
+	// Group output
+	let groupBy;
+	if (flags.g) {
+		groupBy = fields
+			.map((item) => item.toLowerCase())
+			.indexOf(flags.g.toLowerCase());
+		if (groupBy === -1) {
+			console.log(`${logSymbols.error} Invalid Field`);
+			return;
+		}
+	}
 
-    // Group output
-    let groupBy;
-    if (flags.group) {
-      groupBy = fields
-        .map((item) => item.toLowerCase())
-        .indexOf(flags.group.toLowerCase());
-      if (groupBy === -1) {
-        this.log(`${logSymbols.error} Invalid Field`);
-        return;
-      }
-    }
+	// Repeated requests to get all repositories
+	let endCursor,
+		hasNextPage,
+		points,
+		repositories = [];
 
-    // Repeated requests to get all repositories
-    let endCursor,
-      hasNextPage,
-      points,
-      repositories = [];
-
-    do {
-      const {
-        viewer: {
-          repositories: { nodes, pageInfo },
-        },
-        rateLimit,
-      } = await graphql(
-        `
+	do {
+		const {
+			viewer: {
+				repositories: { nodes, pageInfo },
+			},
+			rateLimit,
+		} = await graphql(
+			`
         query {
           viewer {
             repositories(
               first: 100
               affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]
-              ${endCursor ? `after: "${endCursor}"` : ""}
+              ${endCursor ? `after: "${endCursor}"` : ''}
             ) {
               totalCount
               pageInfo {
@@ -85,76 +80,59 @@ class List extends Command {
           }
         }
       `,
-        {
-          headers: {
-            authorization: `token ${process.env.GITHUB_PAT}`,
-          },
-        }
-      );
+			{
+				headers: {
+					authorization: `token ${process.env.GITHUB_PAT}`,
+				},
+			}
+		);
 
-      endCursor = pageInfo.endCursor;
-      hasNextPage = pageInfo.hasNextPage;
-      points = rateLimit;
-      repositories = repositories.concat(nodes);
-    } while (hasNextPage);
+		endCursor = pageInfo.endCursor;
+		hasNextPage = pageInfo.hasNextPage;
+		points = rateLimit;
+		repositories = repositories.concat(nodes);
+	} while (hasNextPage);
 
-    let table;
+	let table;
 
-    // Grouped output
-    if (flags.group) {
-      table = new Table({
-        head: [fields[groupBy], "Repository"],
-      });
+	// Grouped output
+	if (flags.g) {
+		table = new Table({
+			head: [fields[groupBy], 'Repository'],
+		});
 
-      let groupedObj = {};
-      repositories.forEach((item) => {
-        const key = mappedFields[groupBy](item);
-        if (key in groupedObj) {
-          groupedObj[key].push(item.name);
-        } else groupedObj[key] = [item.name];
-      });
+		const groupedObj = {};
+		repositories.forEach((item) => {
+			const key = mappedFields[groupBy](item);
+			if (key in groupedObj) {
+				groupedObj[key].push(item.name);
+			} else { groupedObj[key] = [item.name]; }
+		});
 
-      Object.entries(groupedObj).forEach((item) => {
-        table.push([item[0], item[1].join("\n")]);
-      });
-    } else {
-      table = new Table({
-        head: fields,
-      });
+		Object.entries(groupedObj).forEach((item) => {
+			table.push([item[0], item[1].join('\n')]);
+		});
+	} else {
+		table = new Table({
+			head: fields,
+		});
 
-      repositories.forEach((item) => {
-        table.push([
-          item.name,
-          item.owner.login,
-          item.viewerPermission,
-          item.defaultBranchRef ? item.defaultBranchRef.name : "---",
-          item.isPrivate ? logSymbols.error : logSymbols.success,
-        ]);
-      });
-    }
+		repositories.forEach((item) => {
+			table.push([
+				item.name,
+				item.owner.login,
+				item.viewerPermission,
+				item.defaultBranchRef ? item.defaultBranchRef.name : '---',
+				item.isPrivate ? logSymbols.error : logSymbols.success,
+			]);
+		});
+	}
 
-    this.log(table.toString());
+	console.log(table.toString());
 
-    this.log(`API Points:
+	console.log(`API Points:
     used\t-\t${points.cost}
     remaining\t-\t${points.remaining}`);
-  }
-}
-
-List.description = `This command lists all of the repositories that the current user has.
-NOTE: An environment variable GITHUB_PAT with the personal access token is required.
-Also make sure that the token has read:org and all repo permissions.
-`;
-
-List.flags = {
-  group: flags.string({
-    char: "g",
-    description: "field to be grouped by",
-  }),
-  fields: flags.boolean({
-    char: "f",
-    description: "show available fields",
-  }),
 };
 
-module.exports = List;
+module.exports = list;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-module.exports = require("@oclif/command");
+#!/usr/bin/env node
+
+'use strict';
+
+require('dotenv').config();
+var Yargs = require('yargs');
+var list = require('./commands/list');
+
+var argv = Yargs.usage('Usage: $0 <command> [options]')
+	.command('list', 'lists all repositories', function (yargs) {
+		argv = yargs
+			.usage('Usage: $0 list [options]')
+			.alias('g', 'group-by')
+			.nargs('g', 1)
+			.describe('g', 'Field to be grouped by')
+			.alias('f', 'fields')
+			.boolean('f')
+			.describe('f', 'Show available fields')
+			.help('h')
+			.alias('h', 'help').argv;
+		list(argv);
+	})
+	.help('h')
+	.alias('h', 'help').argv;
