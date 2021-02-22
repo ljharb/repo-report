e933350a294212acf697167420378bbde93336e4
jakazzy <jakazzy@gmail.com>
2021-02-22 06:31:20 +0000

    add common methods among the command defintion files here

---
+++ src/utils.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const listFields = (fields) => fields.map((item) => console.log(`- ${item}`));
+
+const getGroupIndex = (group, fields) => fields.map((item) => item.toLowerCase()).indexOf(group.toLowerCase());
+
+const printAPIPoints = (points) => {
+	console.log(`API Points:
+  \tused\t\t-\t${points.cost}
+  \tremaining\t-\t${points.remaining}`);
+};
+
+const getItemFields = (item) => {
+	const name = item.name;
+	const defBranch = item.defaultBranchRef ? item.defaultBranchRef.name : '---';
+	const { branchProtectionRule } = item.defaultBranchRef || {};
+	const {
+		allowsForcePushes,
+		allowsDeletions,
+		dismissesStaleReviews,
+		restrictsReviewDismissals,
+		requiredApprovingReviewCount,
+		requiresApprovingReviews,
+		requiresCodeOwnerReviews,
+		requiresStatusChecks,
+		requiresStrictStatusChecks,
+		restrictsPushes,
+		pattern,
+	} = branchProtectionRule || {};
+
+	return {
+		allowsDeletions,
+		allowsForcePushes,
+		defBranch,
+		dismissesStaleReviews,
+		name,
+		pattern,
+		requiredApprovingReviewCount,
+		requiresApprovingReviews,
+		requiresCodeOwnerReviews,
+		requiresStatusChecks,
+		requiresStrictStatusChecks,
+		restrictsPushes,
+		restrictsReviewDismissals,
+	};
+};
+
+module.exports = {
+	getGroupIndex,
+	getItemFields,
+	listFields,
+	printAPIPoints,
+};
