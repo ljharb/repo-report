d55ab79f63f6d2e8ebf35ac5790ca4bdeaefbc4d
jakazzy <jakazzy@gmail.com>
2021-04-04 19:42:15 +0000

    update mock

--- test/mocks.js
+++ test/mocks.js
@@ -3,54 +3,60 @@
 'use strict';
 
 const assert = require('assert');
+const nock = require('nock');
 const fetchMock = require('fetch-mock');
 const { graphql } = require('@octokit/graphql');
 
-const mockGetRepositories = (response) => graphql(`query {
-    viewer {
-    repositories(
-      first: 100
-      affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]
-    ) {
-      totalCount
-      pageInfo {
-      endCursor
-      hasNextPage
-      }
-      nodes {
-      name
-      nameWithOwner
-      isPrivate
-      defaultBranchRef {
-        name
-      }
-      viewerPermission
-      }
-    }
-    }
-    rateLimit {
-    cost
-    remaining
-    }
-  }`, {
-	headers: {
-		authorization: 'token secret123',
-	},
-	request: {
-		fetch: fetchMock
-			.sandbox()
-			.post(
-				'https://api.github.com/graphql',
-				{ data: response },
-				{
-					headers: {
-						accept: 'application/vnd.github.v3+json',
-						authorization: 'token secret123',
-					} },
-			),
-	},
-}).then((result) => result);
+/*
+ * const mockGetRepositories = (response) => graphql(`query {
+ *     viewer {
+ *     repositories(
+ *       first: 100
+ *       affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]
+ *     ) {
+ *       totalCount
+ *       pageInfo {
+ *       endCursor
+ *       hasNextPage
+ *       }
+ *       nodes {
+ *       name
+ *       nameWithOwner
+ *       isPrivate
+ *       defaultBranchRef {
+ *         name
+ *       }
+ *       viewerPermission
+ *       }
+ *     }
+ *     }
+ *     rateLimit {
+ *     cost
+ *     remaining
+ *     }
+ *   }`, {
+ * 	headers: {
+ * 		authorization: 'token secret123',
+ * 	},
+ * 	request: {
+ * 		fetch: fetchMock
+ * 			.sandbox()
+ * 			.post(
+ * 				'https://api.github.com/graphql',
+ * 				{ data: response },
+ * 				{
+ * 					headers: {
+ * 						accept: 'application/vnd.github.v3+json',
+ * 						authorization: 'token secret123',
+ * 					} },
+ * 			),
+ * 	},
+ * }).then((result) => result);
+ */
 
+const mockGetRepositories = (Response) => nock('https://api.github.com')
+	.post('/repos/atom/atom/license')
+	.reply(201, Response);
 module.exports = {
 	mockGetRepositories,
 };
