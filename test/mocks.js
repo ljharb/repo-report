7a2fb54f7be1b6e465b0b985ab2914015a9ccf00
jakazzy <jakazzy@gmail.com>
2021-04-06 08:15:35 +0000

    update mock file

--- test/mocks.js
+++ test/mocks.js
@@ -3,60 +3,84 @@
 'use strict';
 
 const assert = require('assert');
-const nock = require('nock');
-const fetchMock = require('fetch-mock');
+const fetchMock = require('fetch-mock/es5/server');
 const { graphql } = require('@octokit/graphql');
 
+const mockGetRepositories = (response) => graphql(`{ viewer {
+  repositories(
+    first: 100
+    affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]
+  ) {
+    totalCount
+    pageInfo {
+    endCursor
+    hasNextPage
+    }
+    nodes {
+    name
+    nameWithOwner
+    isPrivate
+    defaultBranchRef {
+      name
+    }
+    viewerPermission
+    }
+  }
+  }
+  rateLimit {
+  cost
+  remaining
+  }
+} }`, {
+	headers: {
+		authorization: 'token secret123',
+	},
+	request: {
+		fetch: fetchMock
+			.sandbox()
+			.post('https://api.github.com/graphql', (url, options) => {
+				assert.strictEqual(options.headers.authorization, 'token secret123');
+				assert.strictEqual(
+					options.body,
+					`{'query': '{ viewer {
+            repositories(
+              first: 100
+              affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]
+            ) {
+              totalCount
+              pageInfo {
+              endCursor
+              hasNextPage
+              }
+              nodes {
+              name
+              nameWithOwner
+              isPrivate
+              defaultBranchRef {
+                name
+              }
+              viewerPermission
+              }
+            }
+            }
+            rateLimit {
+            cost
+            remaining
+            }
+          } }'}`,
+					'Sends correct query',
+				);
+				return { data: { response } };
+			}),
+	},
+}).then((result) => result);
+
 /*
- * const mockGetRepositories = (response) => graphql(`query {
- *     viewer {
- *     repositories(
- *       first: 100
- *       affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]
- *     ) {
- *       totalCount
- *       pageInfo {
- *       endCursor
- *       hasNextPage
- *       }
- *       nodes {
- *       name
- *       nameWithOwner
- *       isPrivate
- *       defaultBranchRef {
- *         name
- *       }
- *       viewerPermission
- *       }
- *     }
- *     }
- *     rateLimit {
- *     cost
- *     remaining
- *     }
- *   }`, {
- * 	headers: {
- * 		authorization: 'token secret123',
- * 	},
- * 	request: {
- * 		fetch: fetchMock
- * 			.sandbox()
- * 			.post(
- * 				'https://api.github.com/graphql',
- * 				{ data: response },
- * 				{
- * 					headers: {
- * 						accept: 'application/vnd.github.v3+json',
- * 						authorization: 'token secret123',
- * 					} },
- * 			),
- * 	},
- * }).then((result) => result);
+ * const mockGetRepositories = (Response) => nock('https://api.github.com')
+ * 	.post('/graphl')
+ * 	.reply(201, Response);
  */
 
-const mockGetRepositories = (Response) => nock('https://api.github.com/graphl')
-	.post('')
-	.reply(201, Response);
 module.exports = {
 	mockGetRepositories,
 };
