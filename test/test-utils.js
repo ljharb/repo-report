4d5db5579aedcd02083aa7eba30c4a5cd54b4d4f
jakazzy <jakazzy@gmail.com>
2021-04-06 07:56:05 +0000

    update the test utils with the stdout

--- test/test-utils.js
+++ test/test-utils.js
@@ -4,9 +4,28 @@ const { spawnSync } = require('child_process');
 
 const runCli = (command) => spawnSync('node', ['../repo-report/bin/run', ...command], { encoding: 'utf-8' });
 
-const createCLIWrapper = () => ({
+const cliWrapper = () => ({
 	run: runCli,
 });
 
-module.exports = createCLIWrapper;
+const stdout = () => {
+	const previousWrite = process.stdout.write;
+	const loggedData = [];
 
+	process.stdout.write = function (string) {
+		loggedData.push(string);
+	};
+
+	return {
+		loggedData,
+		restore() {
+			process.stdout.write = previousWrite;
+		},
+	};
+
+};
+
+module.exports = {
+	cliWrapper,
+	stdout,
+};
