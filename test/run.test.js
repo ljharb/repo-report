b30d2b51e2fc100d84c48e971f7dd6ad00afe153
jakazzy <jakazzy@gmail.com>
2021-04-06 07:55:37 +0000

    update the test file run.test with the test suit

--- test/run.test.js
+++ test/run.test.js
@@ -4,49 +4,181 @@
 
 const test = require('tape');
 
-const { createCLIWrapper } = require('../test/test-utils');
-const { mockRepositoriesData } = require('./fixtures/fixtures.js');
-const { mockGetRepositories } = require('./mocks.js');
+const { cliWrapper } = require('../test/test-utils');
 
-test('list command', (t) => {
-	t.plan(1);
-	t.test('list command should return users repositories', async (t) => {
-		/*
-		 * const data = mockGetRepositories(mockRepositoriesData);
-		 * console.log(data, '&&&&&&&&&&&&');
-		 */
-		mockGetRepositories(mockRepositoriesData).log(console.log);
-		const wrapper = createCLIWrapper();
+test('list command', async (t) => {
+	t.plan(5);
+	t.test('output to have correct columns', async (t) => {
+		const wrapper = cliWrapper();
 		const result = wrapper.run(['list']);
-		console.log(result.stdout);
+		const tableColumns = ['Repository', 'Access', 'DefBranch'];
+		t.ok(tableColumns.every((column) => result.stdout.includes(column)));
 		t.end();
 	});
 
-	/*
-	 * t.test('list -g field to groupby\' should return output', (t) => {
-	 * 	const wrapper = createCLIWrapper();
-	 * 	const result = wrapper.run(['list']);
-	 * 	console.log(result.stdout);
-	 * });
-	 */
+	t.test('output to have correct rows', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['list', '-g', 'Access']);
+		const tableRows = ['ADMIN', 'WRITE', 'READ', 'MAINTAIN'];
+		t.ok(tableRows.some((row) => result.stdout.includes(row)));
+		t.end();
+	});
 
-	/*
-	 * t.test('\'-f list all the fields', (t) => {
-	 * 	const wrapper = createCLIWrapper();
-	 * 	const result = wrapper.run(['list', '-f']);
-	 */
+	t.test('field output to be correct', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['list', '-f']);
+		const fields = '- Repository\n- Access\n- DefBranch\n- isPrivate\n';
+		t.equal(result.stdout, fields);
+		t.end();
+	});
 
-	/*
-	 * 	console.log(result);
-	 * 	t.end();
-	 * });
-	 * t.test('\'-f list all the fields', (t) => {
-	 * 	const wrapper = createCLIWrapper();
-	 * 	const result = wrapper.run(['list', '-s']);
-	 * 	// console.log(result);
-	 * 	t.end();
-	 * });
-	 */
+	t.test('output should be sorted', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['list', '-s']);
+		// assertion
 
+		// console.log(result.stdout);
+		t.end();
+	});
+
+	t.test('output the help info', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['list', '-h']);
+		const helpInfo = ['--version', '-group-by', '--fields', '--sort', '--help'];
+		t.ok(helpInfo.every((info) => result.stdout.includes(info)));
+		t.end();
+	});
+});
+
+test('options command', async (t) => {
+	t.plan(5);
+	t.test('output to have correct columns', async (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['options']);
+		const tableColumns = ['Repository', 'Wiki', 'Projects', 'securityPolicy', 'mergeCommit', 'squashMerge', 'rebaseMerge', 'deleteOnMerge'];
+		t.ok(tableColumns.every((column) => result.stdout.includes(column)));
+		t.end();
+	});
+
+	t.test('output to have correct rows', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['options', '-g', 'Wiki']);
+		const tableRows = [' ✔', '✖'];
+		t.ok(tableRows.some((row) => result.stdout.includes(row)));
+		t.end();
+	});
+
+	t.test('field output to be correct', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['options', '-f']);
+		const fields = '- Repository\n- Wiki\n- Projects\n- securityPolicy\n- mergeCommit\n- squashMerge\n- rebaseMerge\n- deleteOnMerge\n- isPrivate\n';
+		t.equal(result.stdout, fields);
+		t.end();
+	});
+
+	t.test('output should be sorted', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['options', '-s']);
+		// assertion
+
+		// console.log(result.stdout);
+		t.end();
+	});
+
+	t.test('output the help info', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['options', '-h']);
+		const helpInfo = ['--version', '-group-by', '--fields', '--sort', '--help'];
+		t.ok(helpInfo.every((info) => result.stdout.includes(info)));
+		t.end();
+	});
+});
+
+test('branchProtection command', async (t) => {
+	t.plan(5);
+	t.test('output to have correct columns', async (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['branchProtection']);
+		const tableColumns = ['Repository', 'DefBranch', 'AllowsForcePushes', 'AllowsDeletions', 'DismissesStaleReviews', 'ReqApprovingReviewCount', 'ReqApprovingReviews', 'ReqCodeOwnerReviews'];
+		t.ok(tableColumns.every((column) => result.stdout.includes(column)));
+		t.end();
+	});
+
+	t.test('output to have correct rows', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['branchProtection', '-g', 'Wiki']);
+		const tableRows = [' ✔', '✖'];
+		t.ok(tableRows.some((row) => result.stdout.includes(row)));
+		t.end();
+	});
+
+	t.test('field output to be correct', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['branchProtection', '-f']);
+		const fields = '- Repository\n- DefBranch\n- AllowsForcePushes\n- AllowsDeletions\n- DismissesStaleReviews\n- ReqApprovingReviewCount\n- ReqApprovingReviews\n- ReqCodeOwnerReviews\n- isPrivate\n';
+		t.equal(result.stdout, fields);
+		t.end();
+	});
+
+	t.test('output should be sorted', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['branchProtection', '-s']);
+		// assertion
+
+		// console.log(result.stdout);
+		t.end();
+	});
+
+	t.test('output the help info', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['branchProtection', '-h']);
+		const helpInfo = ['--version', '-group-by', '--fields', '--help'];
+		t.ok(helpInfo.every((info) => result.stdout.includes(info)));
+		t.end();
+	});
+});
+
+test('detail command', async (t) => {
+	t.plan(5);
+	t.test('output to have correct columns', async (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['detail']);
+		const tableColumns = ['Repository', 'Access', 'IssuesEnabled', 'ProjectsEnabled', 'WikiEnabled', 'Archived', 'BlankIssuesEnabled', 'SecurityPolicyEnabled', 'License', 'Merge Strategies', 'DeleteOnMerge', 'HasStarred', 'Subscription', 'DefBranch', 'AllowsForcePushes', 'AllowsDeletions', 'DismissesStaleReviews', 'ReqApprovingReviewCount', 'ReqApprovingReviews', 'ReqCodeOwnerReviews', 'isPrivate'];
+		t.ok(tableColumns.every((column) => result.stdout.includes(column)));
+		t.end();
+	});
+
+	t.test('output to have correct rows', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['detail', '-g', 'Wiki']);
+		const tableRows = [' ✔', '✖'];
+		t.ok(tableRows.some((row) => result.stdout.includes(row)));
+		t.end();
+	});
+
+	t.test('field output to be correct', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['detail', '-f']);
+		const fields = '- Repository\n- DefBranch\n- AllowsForcePushes\n- AllowsDeletions\n- DismissesStaleReviews\n- ReqApprovingReviewCount\n- ReqApprovingReviews\n- ReqCodeOwnerReviews\n- isPrivate\n';
+		t.equal(result.stdout, fields);
+		t.end();
+	});
+
+	t.test('output should be sorted', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['detail', '-s']);
+		// assertion
+
+		// console.log(result.stdout);
+		t.end();
+	});
+
+	t.test('output the help info', (t) => {
+		const wrapper = cliWrapper();
+		const result = wrapper.run(['detail', '-h']);
+		const helpInfo = ['--version', '-group-by', '--fields', '--help'];
+		t.ok(helpInfo.every((info) => result.stdout.includes(info)));
+		t.end();
+	});
 });
 
