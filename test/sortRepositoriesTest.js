be8d9c3adf92e3c400d31576102ea33d82e91bed
Jordan Harband <ljharb@gmail.com>
2025-09-04 15:48:41 -0700

    [Tests] fix sorting tests
    I renamed "updatedDate" etc, but forgot to update the tests

--- test/sortRepositoriesTest.js
+++ test/sortRepositoriesTest.js
@@ -15,47 +15,47 @@ function cloneData() {
 test('Sort by name ASC', (t) => {
 	const repos = cloneData();
 	const actual = sortRepositories(repos, { sort: 'name', desc: false });
-	const expected = [...cloneData()].sort((a, b) => a.name.localeCompare(b.name));
-	t.deepEqual(actual, expected, 'Repos should be sorted by name A → Z');
+	const expected = cloneData().toSorted((a, b) => a.name.localeCompare(b.name));
+	t.deepEqual(actual, expected, 'Repos are sorted by name A → Z');
 	t.end();
 });
 
 test('Sort by name DESC', (t) => {
 	const repos = cloneData();
 	const actual = sortRepositories(repos, { sort: 'name', desc: true });
-	const expected = [...cloneData()].sort((a, b) => b.name.localeCompare(a.name));
-	t.deepEqual(actual, expected, 'Repos should be sorted by name Z → A');
+	const expected = cloneData().toSorted((a, b) => b.name.localeCompare(a.name));
+	t.deepEqual(actual, expected, 'Repos are sorted by name Z → A');
 	t.end();
 });
 
-test('Sort by updatedDate DESC (default)', (t) => {
+test('Sort by updated date DESC (default)', (t) => {
 	const repos = cloneData();
-	const actual = sortRepositories(repos, { sort: 'updatedDate', desc: false });
-	const expected = [...cloneData()].sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
-	t.deepEqual(actual, expected, 'Repos should be sorted by updatedDate newest → oldest');
+	const actual = sortRepositories(repos, { sort: 'updated', desc: false });
+	const expected = cloneData().toSorted((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+	t.deepEqual(actual, expected, 'Repos are sorted by updated newest → oldest');
 	t.end();
 });
 
-test('Sort by updatedDate ASC', (t) => {
+test('Sort by updated date ASC', (t) => {
 	const repos = cloneData();
-	const actual = sortRepositories(repos, { sort: 'updatedDate', desc: true });
-	const expected = [...cloneData()].sort((a, b) => new Date(a.updatedAt) - new Date(b.updatedAt));
-	t.deepEqual(actual, expected, 'Repos should be sorted by updatedDate oldest → newest');
+	const actual = sortRepositories(repos, { sort: 'updated', desc: true });
+	const expected = cloneData().toSorted((a, b) => new Date(a.updatedAt) - new Date(b.updatedAt));
+	t.deepEqual(actual, expected, 'Repos are sorted by updated oldest → newest');
 	t.end();
 });
 
-test('Sort by createdDate DESC', (t) => {
+test('Sort by created date DESC', (t) => {
 	const repos = cloneData();
-	const actual = sortRepositories(repos, { sort: 'createdDate', desc: false });
-	const expected = [...cloneData()].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-	t.deepEqual(actual, expected, 'Repos should be sorted by createdDate newest → oldest');
+	const actual = sortRepositories(repos, { sort: 'created', desc: false });
+	const expected = cloneData().toSorted((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+	t.deepEqual(actual, expected, 'Repos are sorted by created date newest → oldest');
 	t.end();
 });
 
-test('Sort by createdDate ASC', (t) => {
+test('Sort by created date ASC', (t) => {
 	const repos = cloneData();
-	const actual = sortRepositories(repos, { sort: 'createdDate', desc: true });
-	const expected = [...cloneData()].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-	t.deepEqual(actual, expected, 'Repos should be sorted by createdDate oldest → newest');
+	const actual = sortRepositories(repos, { sort: 'created', desc: true });
+	const expected = cloneData().toSorted((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+	t.deepEqual(actual, expected, 'Repos are sorted by created date oldest → newest');
 	t.end();
 });
